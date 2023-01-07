d40042520453f3e7eda28eb94afc2f954ecb9fef
Kevin <101434732+kevinhuytran1128@users.noreply.github.com>
2023-01-06 20:53:36 -0500

    update

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
 import time 
 
+options = Options()
+options.headless = True
+userAgent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5359.125 Safari/537.36"
+options.add_argument(f'user-agent={userAgent}')
+
 PATH = "C:\Program Files (x86)\chromedriver.exe"
-driver = Chrome(PATH)
+
+driver = Chrome(PATH, chrome_options=options)
 
 list_of_cards = []
 
@@ -17,7 +25,8 @@ with open('text_data/prices.txt','w') as f:
 
         time.sleep(1)
 
-        price = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/section[2]/section/div/div[2]/section[2]/section[1]/div/section[2]/span')
+        price = driver.find_element(By.XPATH, '/html/body/div[2]/div/div/section[2]/section/div/div[2]/section[3]/div/section[1]/ul/li[1]/span[2]')
+        #//*[@id="app"]/div/div/section[2]/section/div/div[2]/section[2]/section[1]/div/section[2]/span
         name = driver.find_element(By.XPATH, '//*[@id="app"]/div/div/section[2]/section/div/div[2]/div/h1')
 
         f.write(f"{name.text}: {price.text} \n")
